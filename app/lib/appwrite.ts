706de70919032dccd80316a1efe02fe0f27ea42d
luigiamihaela <luigiamihaelam@gmail.com>
2025-07-13 23:19:25 +0300

    Implemented the first two authentification screens

---
+++ app/lib/appwrite.ts
@@ -0,0 +1,7 @@
+export const appwriteConfig = {
+    endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
+    projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
+    platform: "com.jsm.foodordering",
+    databaseId: '6873d5270038f37a6d95',
+    userCollectionId: '6873d57e0004c40e9219'
+}
